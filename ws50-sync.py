089349892774291442e484928965c02a4918d95c
dynasticorpheus <dynasticorpheus@gmail.com>
2016-02-02 18:50:03 +0100

    move configuration from script to commandline

--- ws50-sync.py
+++ ws50-sync.py
@@ -8,37 +8,36 @@ import time
 import sqlite3
 import requests
 import re
+import argparse
 from datetime import datetime
 
-###############################################################################
 
-EMAIL = "first.last@email.com"                           # EMAIL ADDRESS
-PASSW = "mybigsecret"                                    # PASSWORD
+_AUTHOR_ = 'dynasticorpheus@gmail.com'
+_VERSION_ = "0.2.0"
 
-DATABASE = "/domoticz/domoticz.db"                       # FULL PATH TO DB
+TMPID = 12
+CO2ID = 35
 
-TMPIDX = 10                                              # IDX TEMPERATURE
-CO2IDX = 11                                              # IDX AIR QUALITY
-
-UPDATE = False                                           # FALSE FOR DRY RUN
-
-###############################################################################
-
-VERSION = "0.1.0"
 URL_DATA = "https://healthmate.withings.com/index/service/v2/measure"
 URL_AUTH = "https://account.withings.com/connectionuser/account_login?appname=my2&appliver=c7726fda&r=https%3A%2F%2Fhealthmate.withings.com%2Fhome"
 URL_ASSO = "https://healthmate.withings.com/index/service/association"
 
-AUTH_DATA = "email=" + EMAIL + "&is_admin=&password=" + PASSW
+parser = argparse.ArgumentParser(description='Withings WS-50 Syncer by dynasticorpheus@gmail.com')
+parser.add_argument('-u', '--username', help='username (email) in use with account.withings.com', required=True)
+parser.add_argument('-p', '--password', help='password in use with my.gigaset-elements.com', required=True)
+parser.add_argument('-c', '--co2', help='co2 idx', type=int, required=True)
+parser.add_argument('-t', '--temperature', help='temperature idx', type=int, required=True)
+parser.add_argument('-d', '--database', help='fully qualified name of database-file', required=True)
+parser.add_argument('-n', '--noaction', help='do not update database', action='store_true', required=False)
+args = parser.parse_args()
 
-TMPID = 12
-CO2ID = 35
+AUTH_DATA = "email=" + args.username + "&is_admin=&password=" + args.password
 
 s = requests.Session()
 end = int(time.time())
 
 print
-print "Withings WS-50 Syncer Version " + VERSION
+print "Withings WS-50 Syncer Version " + _VERSION_
 print
 print "[-] Authenticating"
 
@@ -51,13 +50,14 @@ except Exception:
 d = s.cookies.get_dict()
 accountid = re.sub("[^0-9]", "", str(re.search('(?<=accountId)(.*)', response.content)))
 payload = "accountid=" + str(accountid) + "&action=getbyaccountid&appliver=c7726fda&appname=my2&apppfm=web&enrich=t&sessionid=" + d['session_key'] + "&type=-1"
-response = requests.request("POST", URL_ASSO, data=payload)
+response = s.request("POST", URL_ASSO, data=payload)
 r = response.json()
 deviceid = r['body']['associations'][0]['deviceid']
 
-conn = sqlite3.connect(DATABASE, timeout=60)
+conn = sqlite3.connect(args.database, timeout=60)
 c = conn.cursor()
-for row in c.execute('select max(Date) from Meter where DeviceRowID=' + str(CO2IDX)):
+
+for row in c.execute('select max(Date) from Meter where DeviceRowID=' + str(args.co2)):
     if row[0] is None:
         START = 0
     else:
@@ -88,10 +88,10 @@ try:
     count = 0
     for item in data['body']['series']:
         for item2 in reversed(item['data']):
-            print('[-] INSERT INTO Meter VALUES (' + str(CO2IDX) + ',' + str(item2['value']) + ',0,' + "'" + time.strftime(
+            print('[-] INSERT INTO Meter VALUES (' + str(args.co2) + ',' + str(item2['value']) + ',0,' + "'" + time.strftime(
                 '%Y-%m-%d %H:%M:%S', time.localtime(item2['date'])) + "'" + ')')
-            if UPDATE:
-                c.execute('INSERT INTO Meter VALUES (' + str(CO2IDX) + ',' + str(item2['value']) + ',0,' + "'" + time.strftime(
+            if args.noaction is not True:
+                c.execute('INSERT INTO Meter VALUES (' + str(args.co2) + ',' + str(item2['value']) + ',0,' + "'" + time.strftime(
                     '%Y-%m-%d %H:%M:%S', time.localtime(item2['date'])) + "'" + ')')
             count = count + 1
             sys.stdout.write("\033[F")
@@ -102,16 +102,16 @@ except Exception:
     sys.exit()
 
 
-print "[-] Updating database with " + str(count) + " CO2 measurements" + " [" + str(UPDATE).upper() + "]"
+print "[-] Updating database with " + str(count) + " CO2 measurements" + " [" + str(not args.noaction).upper() + "]"
 
 try:
     count = 0
     for item in data2['body']['series']:
         for item2 in reversed(item['data']):
-            print('[-] INSERT INTO Temperature VALUES (' + str(TMPIDX) + ',' + str(item2['value']) + ',0.0,0,0,0.0,0.0,' + "'" + time.strftime(
+            print('[-] INSERT INTO Temperature VALUES (' + str(args.temperature) + ',' + str(item2['value']) + ',0.0,0,0,0.0,0.0,' + "'" + time.strftime(
                 '%Y-%m-%d %H:%M:%S', time.localtime(item2['date'])) + "'" + ')')
-            if UPDATE:
-                c.execute('INSERT INTO Temperature VALUES (' + str(TMPIDX) + ',' + str(item2['value']) + ',0.0,0,0,0.0,0.0,' + "'" + time.strftime(
+            if args.noaction is not True:
+                c.execute('INSERT INTO Temperature VALUES (' + str(args.temperature) + ',' + str(item2['value']) + ',0.0,0,0,0.0,0.0,' + "'" + time.strftime(
                     '%Y-%m-%d %H:%M:%S', time.localtime(item2['date'])) + "'" + ')')
             count = count + 1
             sys.stdout.write("\033[F")
@@ -121,9 +121,9 @@ except Exception:
     conn.close()
     sys.exit()
 
-print "[-] Updating database with " + str(count) + " TEMPERATURE measurements" + " [" + str(UPDATE).upper() + "]"
+print "[-] Updating database with " + str(count) + " TEMPERATURE measurements" + " [" + str(not args.noaction).upper() + "]"
 
-if UPDATE:
+if args.noaction is not True:
     print "[-] Committing and closing database"
     try:
         conn.commit()
