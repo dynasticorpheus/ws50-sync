5977c67294dd99734f8ada4905ecad3e1e466ce0
dynasticorpheus <dynasticorpheus@gmail.com>
2017-07-05 19:00:48 +0200

    python 2 and 3 compatible code

--- ws50-sync.py
+++ ws50-sync.py
@@ -3,18 +3,19 @@
 
 """Retrospective update of Domoticz with Withings data ON DB LEVEL. BE CAREFULL!"""
 
+from datetime import datetime
+
 import os
 import sys
 import time
 import sqlite3
 import argparse
 import hashlib
-from datetime import datetime
 
 import requests
 
 _AUTHOR_ = 'dynasticorpheus@gmail.com'
-_VERSION_ = "0.4.8"
+_VERSION_ = "0.4.9"
 
 parser = argparse.ArgumentParser(description='Withings WS-50 Syncer by dynasticorpheus@gmail.com')
 parser.add_argument('-u', '--username', help='username (email) in use with account.withings.com', required=True)
@@ -22,7 +23,7 @@ parser.add_argument('-p', '--password', help='password in use with account.withi
 parser.add_argument('-c', '--co2', help='co2 idx', type=int, required=False)
 parser.add_argument('-t', '--temperature', help='temperature idx', type=int, required=False)
 parser.add_argument('-d', '--database', help='fully qualified name of database-file', required=True)
-parser.add_argument('-l', '--length', help='set short log length (defaults to one day)', type=int, choices=xrange(1, 8), default=1, required=False)
+parser.add_argument('-l', '--length', help='set short log length (defaults to one day)', type=int, choices=range(1, 8), default=1, required=False)
 parser.add_argument('-f', '--full', help='update using complete history', action='store_true', required=False)
 parser.add_argument('-r', '--remove', help='clear existing data from database', action='store_true', required=False)
 parser.add_argument('-w', '--warning', help='suppress urllib3 warnings', action='store_true', required=False)
@@ -47,11 +48,13 @@ URL_USAGE = "https://goo.gl/z6NNlH"
 
 
 def clear_line():
+    """Clear line."""
     sys.stdout.write("\033[F")
     sys.stdout.write("\033[K")
 
 
 def init_database(db):
+    """Initialize database."""
     global conn
     global c
     if os.path.exists(db):
@@ -61,13 +64,14 @@ def init_database(db):
         dbinfo = c.fetchall()
         for row in dbinfo:
             dbversion = row[1]
-        print "[-] Attaching database " + db + " [version " + str(dbversion) + "]"
+        print("[-] Attaching database " + db + " [version " + str(dbversion) + "]")
     else:
         sys.exit("[-] Database not found " + db + "\n")
 
 
 def clear_devices(idx, table):
-    print "[-] Removing existing data from table " + str(table).upper()
+    """Remove existing data for respective sensor from database."""
+    print("[-] Removing existing data from table " + str(table).upper())
     try:
         c.execute('DELETE FROM ' + str(table) + ' WHERE DeviceRowID = ' + str(idx) + ';')
     except Exception:
@@ -75,6 +79,7 @@ def clear_devices(idx, table):
 
 
 def get_lastupdate(idx, table):
+    """Get date from last update for respective sensor."""
     comment = ""
     for dates in c.execute('select max(Date) from ' + str(table) + ' where DeviceRowID=' + str(idx)):
         if dates[0] is None:
@@ -86,11 +91,12 @@ def get_lastupdate(idx, table):
             if lastdate < PDAY:
                 lastdate = PDAY
                 comment = " (" + str(args.length) + " day limit)"
-    print "[-] Downloading all measurements recorded after " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(lastdate)) + comment
+    print("[-] Downloading all measurements recorded after " + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(lastdate)) + comment)
     return lastdate
 
 
 def authenticate_withings(username, password):
+    """Authenticate based on username and md5 hashed password."""
     global pem
     if args.warning:
         try:
@@ -106,19 +112,20 @@ def authenticate_withings(username, password):
         except Exception:
             pem = True
     requests.head(URL_USAGE, timeout=3, headers=HEADER, allow_redirects=True, verify=pem)
-    payload = {'email': username, 'hash': hashlib.md5(password).hexdigest(), 'duration': '900'}
-    print "[-] Authenticating at scalews.withings.net"
+    payload = {'email': username, 'hash': hashlib.md5(password.encode('utf-8')).hexdigest(), 'duration': '900'}
+    print("[-] Authenticating at scalews.withings.net")
     response = requests.post(URL_AUTH, data=payload)
-    id = response.json()
-    sessionkey = id['body']['sessionid']
+    iddata = response.json()
+    sessionkey = iddata['body']['sessionid']
     response = requests.get(URL_ASSO + sessionkey)
-    id = response.json()
-    deviceid = id['body']['associations'][0]['deviceid']
+    iddata = response.json()
+    deviceid = iddata['body']['associations'][0]['deviceid']
     return deviceid, sessionkey
 
 
-def download_data(deviceid, sessionkey, type, lastdate):
-    payload = '/v2/measure?action=getmeashf&deviceid=' + str(deviceid) + '&meastype=' + str(type) + '&startdate=' + str(lastdate) + '&enddate=' + str(NOW) + \
+def download_data(deviceid, sessionkey, mtype, lastdate):
+    """Download json data from scale based on measurement type."""
+    payload = '/v2/measure?action=getmeashf&deviceid=' + str(deviceid) + '&meastype=' + str(mtype) + '&startdate=' + str(lastdate) + '&enddate=' + str(NOW) + \
         '&appliver=3000201&apppfm=android&appname=wiscaleNG&callctx=foreground&sessionid=' + str(sessionkey)
     try:
         response = requests.get(URL_BASE + payload)
@@ -129,13 +136,14 @@ def download_data(deviceid, sessionkey, type, lastdate):
 
 
 def update_meter(name, idx, field, dbtable, dataset, status=None):
+    """Update database based on newly downloaded data for respective sensor."""
     try:
         count = 0
         for item in dataset['body']['series']:
             for item2 in reversed(item['data']):
                 if not args.quiet:
-                    print('[-] INSERT INTO ' + str(dbtable) + '(DeviceRowID,' + str(field) + ',Date) VALUES (' + str(idx) + ',' + str(
-                        item2['value']) + ",'" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(item2['date'])) + "'" + ')')
+                    print(('[-] INSERT INTO ' + str(dbtable) + '(DeviceRowID,' + str(field) + ',Date) VALUES (' + str(idx) + ',' + str(
+                        item2['value']) + ",'" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(item2['date'])) + "'" + ')'))
                     clear_line()
                 c.execute('INSERT INTO ' + str(dbtable) + '(DeviceRowID,' + str(field) + ',Date) VALUES (' + str(idx) + ',' + str(
                     item2['value']) + ",'" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(item2['date'])) + "'" + ')')
@@ -144,36 +152,38 @@ def update_meter(name, idx, field, dbtable, dataset, status=None):
                 c.execute('UPDATE DeviceStatus SET ' + str(status) + ' = ' + str(item2['value']) + ' WHERE ID = ' + str(idx))
                 c.execute('UPDATE DeviceStatus SET LastUpdate = ' + "'" + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(
                     item2['date'])) + "'" + ' WHERE ID = ' + str(idx))
-            print "[-] Updating " + str(name).upper() + " table with " + str(count) + " measurements" + " [" + str(not args.noaction).upper() + "]"
+            print("[-] Updating " + str(name).upper() + " table with " + str(count) + " measurements" + " [" + str(not args.noaction).upper() + "]")
     except Exception:
         conn.close()
         sys.exit("[-] Meter update failed, exiting" + "\n")
     return count
 
 
-def full_update(name, type, field, table, idx, dataset):
+def full_update(name, mtype, field, table, idx, dataset):
+    """Update database based full data download for respective sensor."""
     try:
         c.execute('CREATE TEMPORARY TABLE IF NOT EXISTS WS50SYNC ([DeviceRowID] BIGINT NOT NULL, [Value] BIGINT, [Temperature] FLOAT, [Date] DATETIME);')
         update_meter(str(name), idx, field, "WS50SYNC", dataset)
     except Exception:
-        print "[-] Temporary table update failed, exiting"
+        print("[-] Temporary table update failed, exiting" + "\n")
         conn.close()
         sys.exit()
-    print "[-] Calculating daily MIN, MAX & AVG values"
+    print("[-] Calculating daily MIN, MAX & AVG values")
     c.execute('select DeviceRowID, min(' + str(field) + '), max(' + str(field) + '), avg(' + str(
         field) + '), date(date) from WS50SYNC where DeviceRowID=' + str(idx) + ' group by date(date);')
     dbdata = c.fetchall()
     for row in dbdata:
-        if type.upper() == "CO2":
+        if mtype.upper() == "CO2":
             c.execute('INSERT INTO ' + str(table) + ' (DeviceRowID,Value1,Value2,Value3,Value4,Value5,Value6,Date) VALUES (' + str(row[0]) + ',' + str(
                 row[1]) + ',' + str(row[2]) + ',0,0,0,0' + ",'" + str(row[4]) + "'" + ')')
-        if type.upper() == "TEMPERATURE":
+        if mtype.upper() == "TEMPERATURE":
             c.execute('INSERT INTO ' + str(table) + ' (DeviceRowID,Temp_Min,Temp_Max,Temp_Avg,Date) VALUES (' + str(row[0]) + ',' + str(row[1]) + ',' + str(
                 row[2]) + ',' + str(row[3]) + ",'" + str(row[4]) + "'" + ')')
 
 
 def commit_database():
-    print "[-] Committing and closing database"
+    """Committ and close database."""
+    print("[-] Committing and closing database" + "\n")
     try:
         conn.commit()
     except Exception:
@@ -185,10 +195,9 @@ def commit_database():
 
 
 def main():
+    """Main program."""
     totalrows = 0
-    print
-    print "Withings WS-50 Syncer Version " + _VERSION_
-    print
+    print("\n" + "Withings WS-50 Syncer Version " + _VERSION_ + "\n")
 
     if not (args.co2 or args.temperature):
         parser.error('argument -c/--co2 and/or -t/--temperature is required')
@@ -197,7 +206,7 @@ def main():
         parser.error('argument -f/--full requires -r/--remove')
 
     if args.noaction:
-        print "[-] Dry run mode enabled, no changes to the database will be made"
+        print("[-] Dry run mode enabled, no changes to the database will be made")
 
     init_database(args.database)
 
@@ -232,10 +241,8 @@ def main():
     if not args.noaction and totalrows > 0:
         commit_database()
     else:
-        print "[-] Nothing to commit, closing database"
+        print("[-] Nothing to commit, closing database" + "\n")
         conn.close()
-
-    print
 
 if __name__ == "__main__":
     main()
